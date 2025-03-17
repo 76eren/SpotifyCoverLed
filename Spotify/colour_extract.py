ff78a0b252ba43eae30a0e23e95e2e2a07747966
Eren <e.basaran2004@gmail.com>
2025-03-17 21:33:59 +0100

    Fix: attempted to force more vibrant colors

--- Spotify/colour_extract.py
+++ Spotify/colour_extract.py
@@ -1,16 +1,28 @@
-from colorthief import ColorThief
-import requests
 import os
+from colorsys import rgb_to_hsv
+import requests
+from colorthief import ColorThief
+
 class ColourExtract:
 
     @staticmethod
+    def is_vibrant(color):
+        r, g, b = color
+        h, s, v = rgb_to_hsv(r / 255.0, g / 255.0, b / 255.0)
+        return s > 0.2 and v > 0.3  # Thresholds to avoid grayscale tones
+
+    @staticmethod
     def get_colour(image_url):
-        # TODO: Update some of this logic
         ColourExtract.download_image(image_url)
-        color_thief = ColorThief(os.path.join(os.path.dirname(__file__), "images", "image.jpg"))
-        dominant_colour = color_thief.get_color(quality=1)
+        image_path = os.path.join(os.path.dirname(__file__), "images", "image.jpg")
+        color_thief = ColorThief(image_path)
+        palette = color_thief.get_palette(color_count=5, quality=1)
         ColourExtract.remove_image()
-        return dominant_colour
+
+        # Prioritize vibrant colors
+        vibrant_colors = [color for color in palette if ColourExtract.is_vibrant(color)]
+
+        return vibrant_colors[0] if vibrant_colors else palette[0]  # Prefer vibrant color if available
 
     @staticmethod
     def download_image(url):
